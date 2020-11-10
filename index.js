ef09e8fc7fd50160b1a03a776cb0f811138982f6
yorchgranada <72442070+yorchgranada@users.noreply.github.com>
2020-11-10 16:25:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function mensajeRespuesta1() {
+    document.getElementById("texto").innerHTML = ("¡Magnífico! Te animamos a conocer a otros autores como Erika Martínez, Andrés Neuman o Luis Rosales");
+}
+function mensajeRespuesta2() {
+    document.getElementById("texto").innerHTML = ("Te animamos a visitar nuestra página para conocer más autores");
+}
