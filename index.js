ca030205820e1c9f1242d6077d66e477b1511d56
yorchgranada <72442070+yorchgranada@users.noreply.github.com>
2020-11-10 14:02:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function mensajeRespuesta1() {
-    alert("¡Magnífico! Te animamos a conocer a otros autores como Erika Martínez, Andrés Neuman o Luis Rosales")
+    alert("¡Magnífico! Te animamos a conocer a otros autores como Erika Martínez, Andrés Neuman o Luis Rosales");
 }
 function mensajeRespuesta2() {
-    alert("Te animamos a visitar nuestra página para conocer más autores")
+    alert("Te animamos a visitar nuestra página para conocer más autores");
 }
